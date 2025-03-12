01cd187879da3284202ba7776dc3bc57e30abcc1
agathamv <agathamartinv@gmail.com>
2025-03-12 11:49:51 +0100

    mogo y app

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const dbConnect = require('./config/mongo');
+const app = express();
+
+//Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
+app.use(cors());
+
+//para utilizar el req.body
+app.use(express.json());
+
+app.use("/api", require("./routes"));
+
+app.use(express.static("storage")) // http://localhost:3000/file.jpg
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Servidor escuchando en el puerto " + port);
+})
+
+dbConnect();
